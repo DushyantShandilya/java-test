7ebd5c82355159f55f127519e8fbe63c264b0894
DushyantShandilya <dushyantshandilya579@gmail.com>
2025-09-25 00:30:40 +0530

    Updated Dushyant.java

---
+++ Dushyant.java
@@ -0,0 +1,12 @@
+public class Dushyant{
+    public static void main(String args[]){
+        String lol = "hello";
+        int a = 5;
+        int b = 10;
+        System.out.println(lol); 
+        System.out.println(a+b);
+
+        a = 50;
+        System.out.println(a+b);
+    } 
+}
